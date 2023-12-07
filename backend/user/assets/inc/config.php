dd20705cb061a5af47a498a48c290c0de20b5b24
jhaisrhieemata <132376886+jhaisrhieemata@users.noreply.github.com>
2023-12-07 21:43:19 +0800

    deleted user

--- backend/user/assets/inc/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="manolo_pesodb";
-$mysqli=new mysqli($host,$dbuser, $dbpass, $db);
-?>
--- backend/user/assets/inc/footer1.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer footer-alt">
-            2023 - <?php echo date ('Y');?> &copy; Management Information System.</a> 
-</footer>
